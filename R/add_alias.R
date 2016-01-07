832977f5f23e80b8096dd14b28cc3e9d92ee5d36
Michael Cysouw <cysouw@mac.com>
2016-01-07 01:04:48 +0100

    adding safeguards

--- R/add_alias.R
+++ R/add_alias.R
@@ -1,19 +1,36 @@
 # helper function to add an alias for executables
 # dangerous for non-terminal people!
 
-add_alias <- function(execs, package = "qlcData", profile = "~/.bash_profile") {
+add_alias <- function(execs, package = "qlcData", shellprofile = "auto") {
+  
+  # which profile to choose
+  if (shellprofile == "auto") {
+    if (file.exists("~/.bash_profile")) {
+      shellprofile = "~/.bash_profile"
+    } else if (file.exists("~/.profile")) {
+      shellprofile = "~/.profile"
+    } else {
+      shellprofile = "~/.bashrc"
+    }
+  }
+  
+  if (!file.exists(shellprofile)) {
+    system2("touch", shellprofile)
+  }
   
   for (e in execs) {
     path <- file.path(find.package(package), "exec", e)
-    system(paste0("echo 'alias "
-                  , e
-                  , "="
-                  , path
-                  , "' >> "
-                  , profile
-    ))		
+    # check whether alias already exists
+    if (system(paste0("grep 'alias\ ", e, "=' ", shellprofile)
+               , ignore.stdout = TRUE) == 1 ) {
+      system(paste0("echo 'alias "
+                    , e
+                    , "="
+                    , path
+                    , "' >> "
+                    , shellprofile
+      ))
+      system(paste("source", shellprofile))
+    }
   }
-  
-  system(paste("source", profile))
-
 }
