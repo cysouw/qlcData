f7213d024ae3675d9144134203d770fdcfc3e30e
Michael Cysouw <cysouw@mac.com>
2017-04-22 18:23:28 +0200

    hide convertTreePhylo()

--- R/asPhylo.R
+++ R/asPhylo.R
@@ -6,7 +6,7 @@ asPhylo <- function(tree, height = 100, fixed.branches = NULL, long.root = NULL,
 	# phylo <- phytools::read.newick(text = newick)
 	
   # this simple function cannot take 'height' and always takes 'fixed = 1'
-  phylo <- convertTreePhylo(tree)
+  phylo <- .convertTreePhylo(tree)
   
 	if (!is.null(fixed.branches)) {
 		phylo$edge.length <- rep(fixed.branches, length = nrow(phylo$edge))
@@ -46,4 +46,24 @@ asPhylo <- function(tree, height = 100, fixed.branches = NULL, long.root = NULL,
 	# phylo$tip.label <- gsub("_", " ", phylo$tip.label)
 	
 	return(phylo)
+}
+
+.convertTreePhylo <- function(tree) {
+  
+  tips <- setdiff(tree[,1], tree[,2])
+  nodes <- unique(c("World", tree[,2]))
+  all <- c(tips, nodes)
+  father <- as.integer(factor(tree[,2],levels = all))
+  child <- as.integer(factor(tree[,1],levels = all))
+  edges <- as.matrix(cbind(father, child))
+  edges <- edges[order(edges[,1], edges[,2]), ]
+  dimnames(edges) <- NULL
+  result <- list(  edge = edges
+                   , Nnode = as.integer(length(nodes))
+                   , tip.label = tips
+                   , edge.length = rep(1,nrow(edges))
+                   , node.label = nodes
+  )
+  class(result) <- "phylo"
+  return(result)
 }
